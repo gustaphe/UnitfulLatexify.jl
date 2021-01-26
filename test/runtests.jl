6701fcf3d6a98010675467b140bca93c7360ef23
David Gustavsson <david.e.gustavsson@gmail.com>
2021-01-26 16:16:49 +0100

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,13 @@ using Test
 
 u = u"J/kg"
 q = 24.7e9u"Gm/s^2"
+n = 6.02214076e23u"one"
 
 @testset "UnitfulLatexify.jl" begin
     @test latexify(u) == L"$\mathrm{J}\mathrm{kg}^{-1}$"
     @test latexify(u,unitformat=:siunitx) == LaTeXString("\\si{\\joule\\per\\kilo\\gram}")
     @test latexify(q) == L"$2.47e10\;\mathrm{Gm}\mathrm{s}^{-2}$"
     @test latexify(q,unitformat=:siunitx) == LaTeXString("\\SI{2.47e10}{\\giga\\meter\\per\\second\\tothe{2}}")
+    @test latexify(n) == L"$6.022 \cdot 10^{23}$"
+    @test latexify(n,unitformat=:siunitx) == LaTeXString("\\num{6.02214076e23}")
 end
